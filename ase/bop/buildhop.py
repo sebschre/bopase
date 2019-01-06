2ef5e46005bbb8d67bdc8cac62512e58a10bb489
Sebastian Schreiber <sebschre@gmail.com>
2019-01-06 19:50:39 +0100

    Rotations of hops

--- ase/bop/buildhop.py
+++ ase/bop/buildhop.py
@@ -1,6 +1,42 @@
+import numpy as np
+from typing import List
 from ase.bop.bopatoms import BOPAtoms
+from ase.neighborlist import NeighborList
+from scipy.spatial.transform import Rotation
 
 
-class TwoCenterHops:
-    def __init__(self, bopatoms: BOPAtoms):
-        pass
+class TwoCenterHoppingIntegrals:
+    def __init__(self, bopatoms: BOPAtoms, cutoffs: list, **kwargs):
+        self.bopatoms = bopatoms
+        self.nl = NeighborList(cutoffs=cutoffs, bothways=True, self_interaction=False, **kwargs)
+        self.nl.update(bopatoms)
+
+    def update_hops(self):
+        raise NotImplemented
+
+    def get_relative_position(self, index: int, jneigh: int):
+        '''
+        :param index: atom index
+        :param jneigh: indexing neighboring atoms of atom index
+        :return:
+        '''
+        # if index > len(self.bopatoms):
+        #     raise IndexError
+        # if jneigh > self.nl.nneighbors - 1:
+        #     raise IndexError
+        (index_list, relative_position_list) = self.nl.get_neighbors(index)
+        return relative_position_list[jneigh]
+
+    def get_rotation(self, index: int, jneigh:int, z_axis_global: np.array=np.array([0, 0, 1])) -> Rotation:
+        rel_pos = self.get_relative_position(index, jneigh)
+        v = np.cross(rel_pos, z_axis_global)
+        sine   = np.abs(v)
+        cosine = np.dot(rel_pos, z_axis_global)
+        if cosine != -1:
+            v_cross = [[0    , -v[2], v[1] ],
+                       [v[2] , 0    , -v[0]],
+                       [-v[1], v[0] , 0]]
+            rotation_matrix = np.eye(3) + v_cross + np.dot(v_cross, v_cross) / (1 + cosine)
+        else:
+            rotation_matrix = np.diag([1, 1, -1])
+        return Rotation.from_dcm(rotation_matrix)
--- ase/test/bop/boptest.py
+++ ase/test/bop/boptest.py
@@ -1,16 +1,26 @@
 import numpy as np
 from ase.bop.bopatoms import BOPAtoms
 from ase.neighborlist import NeighborList
+from ase.bop.buildhop import TwoCenterHoppingIntegrals
 
 
 onsite_level = 0.5
-struc = BOPAtoms(symbols='Fe', positions=[[0, 0, 0]], onsite_levels=[onsite_level], cell=1 * np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]), pbc=(1, 1, 1))
+struc = BOPAtoms(symbols='Fe', positions=[[0., 0., 0.]], onsite_levels=[onsite_level],
+                 cell=1 * np.array([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]]), pbc=(1, 1, 1))
 assert struc[0].onsite_level == onsite_level
 
 print(struc[0].number_valence_electrons)
 
-cutoff = 1.01
-nl = NeighborList([cutoff/2]*len(struc), skin=0.0)
-nl.update(struc)
+cutoff = 0.6
+cutofflist = [cutoff]*len(struc)
 
-print(nl.get_neighbors(0))
+twocenterhops = TwoCenterHoppingIntegrals(bopatoms=struc, cutoffs=cutofflist, skin=0.0)
+print(twocenterhops.nl.get_neighbors(0))
+
+rotnum = 2
+z_axis = np.array([0., 0., 1.])
+z_axis = z_axis / np.linalg.norm(z_axis)
+rel_pos = twocenterhops.get_relative_position(0, rotnum)
+rotated_rel_pos = twocenterhops.get_rotation(0, rotnum, z_axis_global=z_axis).apply(rel_pos)
+print(z_axis)
+print(rotated_rel_pos)
